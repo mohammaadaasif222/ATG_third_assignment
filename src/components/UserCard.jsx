f65fdfde9149a21f1368946305a09a4bcb5a0807
Mohammad Aasif <kasifsaif784@gmail.com>
2023-12-29 21:13:02 +0530

    initial commit

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -2,23 +2,23 @@ import React from "react";
 
 const UserCard = ({ user, select }) => {
   return (
-    <div className="card p-3 py-4 my-3 border-0" style={{boxShadow: 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px'}} onClick={() => select(user?.id)}>
+    <div className=" card d-flex flex-row gap-3 align-items-center p-3 py-4 my-3 border-0" style={{boxShadow: 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px'}} onClick={() => select(user?.id)}>
       <div className="text-center">
         <img
-          src="https://res.cloudinary.com/mae-com-in/image/upload/v1699458800/images_bx6zzs.png"
+          src={user?.avatar||"https://res.cloudinary.com/mae-com-in/image/upload/v1699458800/images_bx6zzs.png"}
           width="100"
           className="rounded-circle"
         />
       </div>
 
-      <div className="text-center mt-3">
+      <div className="mt-3">
         {/* <span className="bg-secondary p-1 px-4 rounded text-white">Pro</span> */}
-        <h5 className="mt-2 mb-0">{user?.profile.username}</h5>
-        <span>{user?.jobTitle}</span>
+        <h5 className="mt-2 mb-0">User Name : {user?.profile.username}</h5>
+        <span className="text-muted" style={{fontSize:'small'}}><strong>Job Title: </strong>{user?.jobTitle}</span>
 
-        <div className="px-4 mt-1">
-          <p className="fonts">
-           {user?.Bio}
+        <div className="mt-1">
+          <p className="fonts" style={{fontSize:'small'}}>
+          <strong>Bio: </strong> {user?.Bio}
           </p>
         </div>
         <div className="buttons">
